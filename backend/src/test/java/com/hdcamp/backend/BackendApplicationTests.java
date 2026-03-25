a66b768b28195454270600906373add339d92eeb
tiendat3008 <trantiendat1679@gmail.com>
2026-03-26 01:32:18 +0700

    first commit

---
+++ backend/src/test/java/com/hdcamp/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hdcamp.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
